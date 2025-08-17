ed19883162d294b3c59bcad2974f787ed79e9ee8
hemozeetah <ayhamdabah@gmail.com>
2025-08-17 23:35:59 +0300

    implement an interactive prompt to parse statements

--- main.go
+++ main.go
@@ -1,4 +1,11 @@
 package main
 
+import (
+	"os"
+
+	"github.com/hemozeetah/zeta/repl"
+)
+
 func main() {
+	repl.Start(os.Stdin, os.Stdout)
 }
---
+++ repl/repl.go
@@ -0,0 +1,38 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/hemozeetah/zeta/lexer"
+	"github.com/hemozeetah/zeta/parser"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		p := parser.New(lexer.New(line))
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printErrors(out, p.Errors())
+			continue
+		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
