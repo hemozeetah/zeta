dda1e38d5f8844f00fd38046f2fabe539dff3c57
hemozeetah <ayhamdabah@gmail.com>
2025-08-28 09:53:07 +0300

    add more builtin functions

--- evaluator/builtin.go
+++ evaluator/builtin.go
@@ -37,4 +37,77 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	},
+
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return object.NewError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return object.NewError("argument to `first` must be ARRAY, got %s", object.ObjectMap[args[0].Type()])
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+			return object.NULL
+		},
+	},
+
+	"last": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return object.NewError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return object.NewError("argument to `last` must be ARRAY, got %s", object.ObjectMap[args[0].Type()])
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				return arr.Elements[length-1]
+			}
+			return object.NULL
+		},
+	},
+
+	"tail": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return object.NewError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return object.NewError("argument to `rest` must be ARRAY, got %s", object.ObjectMap[args[0].Type()])
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, arr.Elements[1:length])
+				return &object.Array{Elements: newElements}
+			}
+			return object.NULL
+		},
+	},
+
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return object.NewError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return object.NewError("argument to `push` must be ARRAY, got %s", object.ObjectMap[args[0].Type()])
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			newElements := make([]object.Object, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+			return &object.Array{Elements: newElements}
+		},
+	},
 }
